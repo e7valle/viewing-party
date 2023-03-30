fc84657b7cfe42adf6af04a9fab273103ed5ff1d
Elizabeth Valle <e7valle@gmail.com>
2023-03-29 17:56:54 -0700

    Passed wave 1, working on wave 2

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -1,11 +1,45 @@
 # ------------- WAVE 1 --------------------
 
 def create_movie(title, genre, rating):
-    pass
+    if title and genre and rating:
+        return {"title": title, "genre": genre, "rating":rating}
+        
+    else:
+        return None 
+    
+# This function 
+def add_to_watched(user_data, movie):
+    watched_list = user_data["watched"]
+    watched_list.append(movie)
+    return user_data
+
+def add_to_watchlist(user_data, movie):
+    watchlist = user_data["watchlist"]
+    watchlist.append(movie)
+    return user_data
+
+def watch_movie(user_data, title):
+    for movie in user_data["watchlist"]:
+        if movie["title"] == title:
+            user_data["watchlist"].remove(movie)
+            user_data["watched"].append(movie)
+    return user_data
 
 # -----------------------------------------
 # ------------- WAVE 2 --------------------
 # -----------------------------------------
+def get_watched_avg_rating(user_data): 
+    if len(user_data["watched"]) < 1:
+        return 0.0
+    
+    rating = 0
+    for movie in user_data["watched"]:
+        rating += movie["rating"]
+    avg_rating = rating/len(user_data["watched"])
+    return float(avg_rating)
+
+def get_most_watched_genre(user_data):
+    
 
 
 # -----------------------------------------
